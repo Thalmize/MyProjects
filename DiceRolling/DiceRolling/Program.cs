5692a844a425661841b09cd3b328ea06b4457a91
Thalmize <Thalmize@gmail.com>
2022-09-19 00:37:22 -0500

    Dice Rolling App

---
+++ DiceRolling/DiceRolling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Console;
+
+namespace DiceRolling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*NEEDS
+             * - Allow user to input number of sides on dice, and how many times it needs to be rolled
+             * - Program should simulate dice rolls and track how many times each number comes up
+             * SUBGOALS
+             * - user prompt for incorrect entry
+             * - program needs to loop, or have loop ability
+             * - print out how many times each side appeared, also print percentage it appeared. Round to 2 dec places
+             * BONUS
+             * - board game dice, create program that draws 2 six sided dice
+             * - allow user to quit or roll again
+             * - allow user to select number of dice to be drawn on screen: (1-4), add dice and display them
+             * 
+             */
+            var menu = new Menu();
+            ///////////////////////////////////////////////
+            //Menu options
+            WriteLine("Welcome!");
+            WriteLine("Choose your option below: ");
+            WriteLine("1 - Simulation Dice Rolling");
+            WriteLine("2 - Board Game Dice");
+            Write("Which option would you like: ");
+            byte input = Convert.ToByte(ReadLine());
+            switch(input)
+            {
+                case 1:
+                    menu.SimulationDiceRolls();
+                    break;
+                case 2:
+                    menu.BoardGameDice();
+                    break;
+            }
+            //initate rolling prompt
+           
+            
+        }
+    }
+}
---
+++ DiceRolling/DiceRolling/Simulation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace DiceRolling
+{
+    class Simulation
+    {
+        public int[] Percentage;
+        public Simulation() { }
+
+        public int[] SimulateDiceRoll(int diceSides, int diceRolls) 
+        {
+            int[] resultOfRoll = new int[diceRolls];
+            Percentage =  new int[diceSides + 1];
+
+            var rand = new Random();
+            int timesRolled = 0;
+            while (timesRolled < diceRolls)
+            {
+                resultOfRoll[timesRolled] = rand.Next(1, diceSides + 1);
+                Percentage[0] = timesRolled;
+                Percentage[resultOfRoll[timesRolled]]++;
+                timesRolled++;
+            }
+            return resultOfRoll;
+        }
+    }
+}
